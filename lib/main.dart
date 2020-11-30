17dd4b9f152a5c353cf0e91a961495ee52a654d9
Sleqit <cartoon_net_nur@mail.ru>
2020-11-30 18:25:35 +0600

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:school_up/shared/group.dart';
+import 'package:school_up/DB/Database.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    DbHelper helper = DbHelper();
+    helper.testDb();
     return MaterialApp(
       title: 'Flutter Demo',
       home: Group(),
